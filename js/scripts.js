f40701703f1a80934bf1c9af5558f5b63d7ca85d
Ben Russell <benjaminrussell36@gmail.com>
2020-06-15 07:26:55 -0700

    Add logic to find contacts and delete contacts.

--- js/scripts.js
+++ js/scripts.js
@@ -21,5 +21,14 @@ AddressBook.prototype.findContact = function(id) {
 }
 
 AddressBook.prototype.deleteContact = function(id) {
-  
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
 }
+
