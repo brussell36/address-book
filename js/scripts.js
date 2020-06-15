fd4362bdb4d4417663e504410313f7c480ab8d01
Ben Russell <benjaminrussell36@gmail.com>
2020-06-15 07:23:25 -0700

    Add business logic to address book to add contacts and assign unique ids.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contact.push(contact);
+}
+
+AddressBook.prototype.findContact = function(id) {
+  for (let i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  
+}
