2d40b201043f267381a94b4a1e04e9a54111371e
Ben Russell <benjaminrussell36@gmail.com>
2020-06-15 07:30:12 -0700

    Add business logic for contacts to return a concatenated name.

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,13 @@ AddressBook.prototype.deleteContact = function(id) {
   return false;
 }
 
+// Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
